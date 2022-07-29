964e3f686f4781c70ea775145e0d24f183268266
Jai <jpate661@my.centennialcollege.ca>
2022-07-28 22:44:35 -0400

    Removed codeLibrary and Added Models in codeAPI

---
+++ codeAPI/Models/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace codeLibrary.Models
+{
+    public partial class Client
+    {
+        public Client()
+        {
+            ClientTutorials = new HashSet<ClientTutorial>();
+        }
+
+        public int ClientId { get; set; }
+        public string ClientEmail { get; set; }
+        public string ClientPassword { get; set; }
+        public string ClientName { get; set; }
+        public string ClientPhone { get; set; }
+        public string ClientAddress { get; set; }
+        public string ClientPostalzip { get; set; }
+        public int TutorialId { get; set; }
+
+        public virtual Tutorial Tutorial { get; set; }
+        public virtual ICollection<ClientTutorial> ClientTutorials { get; set; }
+    }
+}
---
+++ codeAPI/Models/TutorialComment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace codeLibrary.Models
+{
+    public partial class TutorialComment
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public int? TutorialId { get; set; }
+        public string TutorialComment1 { get; set; }
+        public DateTime? TutorialCommentTimestamp { get; set; }
+
+        public virtual Tutorial Tutorial { get; set; }
+    }
+}
